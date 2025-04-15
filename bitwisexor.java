a4c5f158e62b862f2a770206179c583c35b0124e
harshs038 <harsh.s.tech@gmail.com>
2025-04-15 06:31:09 +0530

    bitwise xor

---
+++ bitwisexor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bitwisexor {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Showcasing bitwise and operator.\n");
+        // working of this operator is given below
+        // exmaple 12 and 13 so, 12 binary is 1100 and 13 is 1101
+        // 1100 | 1101 = 0000 so, the answer is : 1
+        // it is convert 1 1 to 0 and 0 0 to 1
+        System.out.print("Please Enter first number : ");
+        int n1 = input.nextInt();
+        System.out.print("Please Enter second number :");
+        int n2 = input.nextInt();
+        int res = n1 ^ n2;
+        System.out.println("Result is : " + res);
+    }
+}
