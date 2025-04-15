4b7af089b13864a39937a49771121396694bcc8e
harshs038 <harsh.s.tech@gmail.com>
2025-04-15 06:23:39 +0530

    variables example

---
+++ variables.java
@@ -0,0 +1,27 @@
+public class variables {
+    public static void main(String[] args) {
+        // variables declration program
+        int number;
+        number = 865;
+        System.out.println(number);
+        // here we can update the value of a variable and print the latest value
+        number = 45;
+        System.out.println(number);
+
+        // float datatype
+        float age = 18.0f;
+        System.out.println(age);
+
+        // double datatype (f is not required in duble datatype)
+        double age2 = 18.0;
+        System.out.println(age2);
+
+        // string datatype always in double cores
+        String name = "HARSH";
+        System.out.println(name);
+
+        // boolean datatype
+        boolean age18 = true;
+        System.out.println(age18);
+    }
+}
