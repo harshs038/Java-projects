9f1425d47a7e876d23b6580cf868f7d12939d6e3
harshs038 <harsh.s.tech@gmail.com>
2025-04-15 06:32:09 +0530

    dowhile

---
+++ dowhile.java
@@ -0,0 +1,9 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Hello World " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
