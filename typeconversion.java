ab6fccb5cadc891282f595761341a936c9bc044a
harshs038 <harsh.s.tech@gmail.com>
2025-04-15 06:22:10 +0530

    type conversion

---
+++ typeconversion.java
@@ -0,0 +1,11 @@
+public class typeconversion {
+    public static void main(String[] args) {
+        float number = 5;
+        System.out.println(number);
+        // here the answer is 5.0 because type conversion
+        // 5l here the meaning of l is long but answer is same
+        // here for convert float into int is given below
+        int number1 = (int) 5.0;
+        System.out.println(number1);
+    }
+}
